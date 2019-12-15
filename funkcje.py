97258fb9176cffa2da3195e852b836ce83b9f1d4
Jerzy <jgrynczewski@gmail.com>
2019-12-15 15:25:10 +0100

    Testowanie wyjątków

--- funkcje.py
+++ funkcje.py
@@ -1,3 +1,5 @@
+import math
+
 def add(x, y):
     return x+y
 
@@ -8,3 +10,12 @@ def product(x, y):
 
 def is_palindrom(str1):
     return str1.lower().replace(" ", "") == str1[::-1].lower().replace(" ", "")
+
+
+def circle_area(r):
+    if r < 0:
+        raise ValueError("The radius cannot be negative.")
+    if type(r) not in [int, float]:
+        raise TypeError("The radius be a non-negative real number.")
+
+    return math.pi * r**2
--- test_funkcje.py
+++ test_funkcje.py
@@ -1,4 +1,5 @@
 import funkcje
+import pytest
 
 
 def test_add():
@@ -17,3 +18,16 @@ def test_is_palindrom():
     assert funkcje.is_palindrom("asdsa")
     assert funkcje.is_palindrom("")
     assert funkcje.is_palindrom("Kobyła ma Mały Bok")
+
+def test_area():
+    assert funkcje.circle_area(1) == funkcje.math.pi
+    assert funkcje.circle_area(0) == 0
+    assert funkcje.circle_area(2.1) == funkcje.math.pi* (2.1**2)
+
+def test_values():
+    with pytest.raises(ValueError):
+        funkcje.circle_area(-2)
+
+def test_type():
+    with pytest.raises(TypeError):
+        funkcje.circle_area("asd")
